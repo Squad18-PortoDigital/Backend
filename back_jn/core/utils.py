510d20c9e30067c11e91a5c47a7ba5552a6f4cbf
Luan Cortes <luancortes208@gmail.com>
2025-04-10 16:47:06 -0300

    feat(utils): Cria função utilitária que desenha e gera o certificado

--- back_jn/core/utils.py
+++ back_jn/core/utils.py
@@ -4,6 +4,15 @@ import uuid
 import requests
 from openai import OpenAI
 from django.conf import settings
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import landscape, A4
+from reportlab.lib.colors import black, HexColor
+from reportlab.platypus import Paragraph, Frame
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
+from django.http import FileResponse
+from io import BytesIO
+
 
 def upload_video_to_s3(file, filename):
     s3 = boto3.client(
@@ -86,3 +95,95 @@ Responda **somente** com um JSON válido. **Não adicione explicações, título
     return response.choices[0].message.content.strip()
 
 
+def gerar_certificado(nome_aluno, nome_curso, carga_horaria, data_inicio, data_conclusão):
+    buffer = BytesIO()
+    c = canvas.Canvas(buffer, pagesize=landscape(A4))
+    width, height = landscape(A4)
+
+    c.setFillColor(HexColor("#efede9"))
+    c.rect(0, 0, width, height, fill=1, stroke=0)
+
+    c.setLineWidth(15)
+    c.setStrokeColor(HexColor("#85211d"))
+    c.rect(40, 20, width - 80, height - 45, fill=0, stroke=1)
+
+    c.setLineWidth(7)
+    c.setStrokeColor(HexColor("#85211d"))
+    c.rect(60, 40, width - 120, height - 90, fill=0, stroke=1)
+
+    c.setLineWidth(2)
+    c.setStrokeColor(HexColor("#85211d"))
+    c.rect(69, 50, width - 140, height - 110, fill=0, stroke=1)
+
+    estilo_Certificado_Conclusao = ParagraphStyle(
+        name = "Título de conclusão",
+        fontName="Helvetica-Bold",
+        fontSize = 35,
+        alignment = TA_CENTER,
+        textColor = HexColor("#85211d")
+    )
+
+    Titulo1 = "Certificado de Conclusão".upper()
+    paragrafo_conclusao = Paragraph(Titulo1, estilo_Certificado_Conclusao)
+    frame_Titulo = Frame(x1= width / 2 - 420, y1= height/2 + 125, width=width, height=50)
+    frame_Titulo.addFromList([paragrafo_conclusao], c)
+
+    estilo_texto_afirmacao = ParagraphStyle(
+        name = "Texto de afirmação",
+        fontName="Helvetica",
+        fontSize = 20,
+        alignment = TA_CENTER,
+    )
+
+    Texto1 = "Certificamos que"
+    paragrafo_texto_afirmacao = Paragraph(Texto1, estilo_texto_afirmacao)
+    frame_Titulo = Frame(x1= width / 2 - 420, y1= height/2 + 65, width=width, height=50)
+    frame_Titulo.addFromList([paragrafo_texto_afirmacao], c)
+
+    estilo_nome_aluno_curso = ParagraphStyle(
+        name = "Nome do aluno",
+        fontName="Helvetica-Bold",
+        fontSize = 35,
+        alignment = TA_CENTER,
+    )
+
+    paragrafo_nome_aluno = Paragraph(nome_aluno, estilo_nome_aluno_curso)
+    frame_Titulo = Frame(x1= width / 2 - 420, y1= height/2 + 35, width=width, height=50, bottomPadding=15)
+    frame_Titulo.addFromList([paragrafo_nome_aluno], c)
+
+    texto2 = "concluiu com êxito o curso"
+    paragrafo_texto_afirmacao2 = Paragraph(texto2, estilo_texto_afirmacao)
+    frame_Titulo = Frame(x1= width / 2 - 420, y1= height/2 - 5, width=width, height=50, topPadding=15, bottomPadding=15)
+    frame_Titulo.addFromList([paragrafo_texto_afirmacao2], c)
+
+    paragrafo_nome_curso = Paragraph(nome_curso, estilo_nome_aluno_curso)
+    frame_Titulo = Frame(x1= width / 2 - 420, y1= height/2 - 35, width=width, height=50, topPadding=15)
+    frame_Titulo.addFromList([paragrafo_nome_curso], c)
+
+    estilo_justificado = ParagraphStyle(
+    name="Justificado",
+    fontName="Helvetica",
+    fontSize=16,
+    leading=16,
+    alignment=TA_JUSTIFY
+)
+    
+    texto3 = (
+    f"Promovido por Jotanunes Construtora, com carga horária de {carga_horaria}. "
+    f"Durante o período de {data_inicio} a {data_conclusão}, foram abordados conteúdos fundamentais para o "
+    f"desenvolvimento na área de {nome_curso}.")
+    paragrafo = Paragraph(texto3, estilo_justificado)
+    frame_descricao = Frame(x1=100, y1=height/2 - 130, width=width - 200, height=80, showBoundary=0)
+    frame_descricao.addFromList([paragrafo], c)
+
+    texto4 = (
+    "Este certificado é conferido em reconhecimento à "
+    "<br/> dedicação e ao cumprimento dos requisitos estabelecidos.")
+    paragrafo2 = Paragraph(texto4, estilo_justificado)
+    frame2 = Frame(x1= 100, y1=height/2 - 220, width=width - 200, height=80, showBoundary=0)
+    frame2.addFromList([paragrafo2], c)
+
+    c.save()
+
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename=f"{nome_aluno}_{nome_curso}.pdf")
