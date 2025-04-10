c93669201ee50ef8a79b52cb6e3510efc6cba07f
Luan Cortes <luancortes208@gmail.com>
2025-04-10 16:49:33 -0300

    feat(urls): Adiciona rota para certificados no router

--- back_jn/core/urls.py
+++ back_jn/core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProfileViewSet, UploadVideoView, VideoViewSet
+from .views import UserViewSet, ProfileViewSet, UploadVideoView, VideoViewSet, CertificadoViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'profiles', ProfileViewSet, basename='profiles')
 router.register(r'videos', VideoViewSet, basename='videos')
+router.register(r'certificados', CertificadoViewSet, basename='certificados')
 
 urlpatterns = [
     path('', include(router.urls)),  # Incluindo as rotas do DRF
